96d6133f2847dfee70939195e3af827d8f5252a3
Nikita <nikitaa1262@gmail.com>
2023-12-31 18:38:23 +0530

    creating user interface on main page

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://s3.amazonaws.com/open-to-cors/assignment.json')
+        .then(response => response.json())
+        .then(data => {
+            const products = data.products;
+
+            // Convert object to array and sort by descending popularity
+            const sortedProducts = Object.keys(products)
+                .map(key => ({ id: key, ...products[key] }))
+                .sort((a, b) => b.popularity - a.popularity);
+
+            displayProducts(sortedProducts);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
+
+function displayProducts(products) {
+    const tableBody = document.getElementById('productBody');
+
+    products.forEach(product => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${product.title}</td>
+            <td>${product.price}</td>
+            <td>${product.popularity}</td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
